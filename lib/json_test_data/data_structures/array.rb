3dc7e581e4bd8140358cdf4456f173b57e889795
danascheider <dscheider@renewfinancial.com>
2016-04-10 17:25:20 -0700

    Define initial .create method

--- lib/json_test_data/data_structures/array.rb
+++ lib/json_test_data/data_structures/array.rb
@@ -2,7 +2,8 @@ module JsonTestData
   class Array
     class << self
       def create(schema)
-        []
+        min = schema.fetch(:minItems, 0)
+        ::Array.new(min)
       end
     end
   end
