fb0425f6283e792090dfa9c7e36cb4dd897e9b14
danascheider <dscheider@renewfinancial.com>
2016-04-21 11:18:05 -0700

    More tests for adjust_for_maximum method

--- spec/json_test_data/data_structures/helpers/number_helper_spec.rb
+++ spec/json_test_data/data_structures/helpers/number_helper_spec.rb
@@ -11,9 +11,21 @@ describe JsonTestData::NumberHelper do
     end
 
     context "number less than maximum" do
+      let(:number) { 3 }
+      let(:maximum) { 4 }
+
+      it "returns the given number" do
+        expect(adjust_for_maximum(number: number, maximum: maximum)).to eql number
+      end
     end
 
     context "number greater than or equal to maximum" do
+      let(:number) { 3 }
+      let(:maximum) { 3 }
+
+      it "returns one less than the maximum" do
+        expect(adjust_for_maximum(number: number, maximum: maximum)).to eql maximum - 1
+      end
     end
   end
 end
