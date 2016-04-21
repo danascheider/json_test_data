15a2c51c9186591e31f551b086bdfc1fa527c083
danascheider <dscheider@renewfinancial.com>
2016-04-21 11:20:47 -0700

    Final test for adjust_for_maximum method

--- spec/json_test_data/data_structures/helpers/number_helper_spec.rb
+++ spec/json_test_data/data_structures/helpers/number_helper_spec.rb
@@ -39,5 +39,17 @@ describe JsonTestData::NumberHelper do
         ).to eql number - step_size
       end
     end
+
+    context "step size less than 1" do
+      let(:number) { 3 }
+      let(:maximum) { 3 }
+      let(:step_size) { 0.25 }
+
+      it "returns the number reduced by step size" do
+        expect(
+          adjust_for_maximum(number: number, maximum: maximum, step_size: step_size)
+        ).to eql number - step_size
+      end
+    end
   end
 end
