0c7823ef91324510b51566a846d7826aa12f1bf8
danascheider <dscheider@renewfinancial.com>
2016-04-23 10:10:35 -0700

    Symbolize keys in Ruby hashes

--- lib/json_test_data.rb
+++ lib/json_test_data.rb
@@ -6,6 +6,6 @@ require_relative './json_test_data/json_schema'
 module JsonTestData
   def self.generate!(schema, opts={})
     schema = JsonSchema.new(schema).generate_example
-    opts[:ruby] ? JSON.parse(schema) : schema
+    opts[:ruby] ? JSON.parse(schema, symbolize_names: true) : schema
   end
 end
