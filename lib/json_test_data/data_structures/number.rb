4f1d9d37d58f2206c2f451ba8b00a4c0f85c24cb
danascheider <dscheider@renewfinancial.com>
2016-04-25 08:14:17 -0500

    Begin going about this in a more OO way

--- lib/json_test_data/data_structures/number.rb
+++ lib/json_test_data/data_structures/number.rb
@@ -9,17 +9,34 @@ module JsonTestData
         factor = schema.fetch(:multipleOf, nil)
         minimum, maximum = schema.fetch(:minimum, nil), schema.fetch(:maximum, nil)
 
+        num = new(min: minimum, max: maximum, factor: factor).value
+
         num = if maximum && minimum
                 between(min: minimum, max: maximum, integer: schema.fetch(:type) == "")
               else
-                factor || 1
+                num
               end
 
         step_size = schema.fetch(:type) == "integer" ? 1 : 0.5
 
+        num = ensure_multiple_of(num, factor) if factor
+
         num = adjust_for_maximum(number: num, maximum: maximum, step_size: factor || step_size)
         adjust_for_minimum(number: num, minimum: minimum, step_size: factor || step_size)
       end
+
+      private
+
+      def ensure_multiple_of(number, factor=1)
+        number % factor == 0 ? number : number * factor
+      end
+    end
+
+    attr_accessor :value
+
+    def initialize(min: minimum, max: maximum, factor: nil, value: nil)
+      @factor, @minimum, @maximum = factor, min, max
+      @value = value || @factor || 1
     end
   end
 end
