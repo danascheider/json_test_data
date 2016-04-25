7488a39f1e0c3907ddfd61df498da8e01f692ee9
danascheider <dscheider@renewfinancial.com>
2016-04-25 10:37:36 -0500

    Get rid of problematic number_helper methods

--- lib/json_test_data/data_structures/helpers/number_helper.rb
+++ lib/json_test_data/data_structures/helpers/number_helper.rb
@@ -1,19 +1,5 @@
 module JsonTestData
   module NumberHelper
-    def adjust_for_maximum(number:, maximum: nil, step_size: nil)
-      return number unless maximum && number >= maximum
-
-      num = adjust(number: number, cutoff: maximum, step_size: step_size && step_size * -1 || -1)
-      adjust_for_maximum(number: num, maximum: maximum, step_size: step_size)
-    end
-
-    def adjust_for_minimum(number:, minimum: nil, step_size: nil)
-      return number unless minimum && number <= minimum
-
-      num = adjust(number: number, cutoff: minimum, step_size: step_size)
-      adjust_for_minimum(number: num, minimum: minimum, step_size: step_size)
-    end
-
     def between(min:, max:, integer: false)
       return integer ? mean(min, max).round(0) : mean(min, max)
     end
