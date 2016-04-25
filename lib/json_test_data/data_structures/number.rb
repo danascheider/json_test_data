5d073d97020c1b8418d1ba1d6bdfef2ce7970385
danascheider <dscheider@renewfinancial.com>
2016-04-25 10:36:33 -0500

    Finish refactor of number

--- lib/json_test_data/data_structures/number.rb
+++ lib/json_test_data/data_structures/number.rb
@@ -9,7 +9,7 @@ module JsonTestData
         factor = schema.fetch(:multipleOf, nil)
         minimum, maximum = schema.fetch(:minimum, nil), schema.fetch(:maximum, nil)
 
-        num = new(min: minimum, max: maximum, factor: factor, type: schema.fetch(:type, "integer").to_sym)
+        num = new(min: minimum, max: maximum, factor: factor, type: schema.fetch(:type, "number").to_sym)
         num.adjust!
       end
 
@@ -20,14 +20,23 @@ module JsonTestData
       end
     end
 
-    attr_accessor :value, :maximum, :minimum, :type, :factor, :step_size
+    attr_accessor :value, :maximum, :minimum, :type, :factor
 
     def initialize(min: nil, max: nil, factor: nil, value: nil, type: nil)
       @factor, @minimum, @maximum = factor, min, max
       @value = value || @factor || 1
-      @type ||= :integer
+      @type ||= :number
+    end
 
-      @step_size = type == :integer ? 1 : 0.5
+    def step_size
+      puts "NUMBER: #{value}"
+      puts "MAXIMUM: #{maximum}"
+      puts "MINIMUM: #{minimum}"
+      puts "STEP SIZE: #{@step_size}"
+      puts ""
+      return @step_size ||= is_int? ? 1 : 0.5 unless minimum && maximum
+
+      @step_size ||= Number.between(min: minimum, max: maximum, integer: type == :integer) / 3
     end
 
     def ensure_multiple_of!
@@ -35,37 +44,42 @@ module JsonTestData
     end
 
     def adjust_for_divisibility!
-      value *= factor unless value_divisible_by_factor?
+      return if value_divisible_by_factor?
+      @value *= factor
+    end
+
+    def is_int?
+      type == :integer
     end
 
     def value_too_low?
       return false unless minimum
-      value <= minimum
+      @value <= minimum
     end
 
     def value_too_high?
       return false unless maximum
-      value >= maximum
+      @value >= maximum
     end
 
     def value_divisible_by_factor?
       return true unless factor
-      value % factor == 0
+      @value % factor == 0
     end
 
     def should_be_int_but_isnt?
-      type == :integer && !value.is_a?(Integer)
+      type == :integer && !@value.is_a?(Integer)
     end
 
     def adjust!
       while !value_divisible_by_factor? || value_too_low? || value_too_high? || should_be_int_but_isnt?
         adjust_for_divisibility!
 
-        if too_high?
+        if value_too_high?
           @value -= step_size
         end
 
-        if too_low?
+        if value_too_low?
           @value += step_size
         end
       end
